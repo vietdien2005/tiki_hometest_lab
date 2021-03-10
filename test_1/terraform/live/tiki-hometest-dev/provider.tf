1e1ed4af722c336436279631a39df1b5201d670e
Dam Viet <vietdien2005@gmail.com>
2021-03-10 23:03:45 +0700

    update key service account

--- test_1/terraform/live/tiki-hometest-dev/provider.tf
+++ test_1/terraform/live/tiki-hometest-dev/provider.tf
@@ -2,14 +2,14 @@ terraform {
   backend "gcs" {
     bucket      = "cp-tf-state"
     prefix      = "hometest.tfstate"
-    credentials = "~/.config/gcloud/hometest_sa.json"
+    credentials = "~/.config/gcloud/hometest-tf-admin.json"
   }
 
   required_version = ">= 0.14"
 }
 
 provider "google" {
-  credentials = file("~/.config/gcloud/hometest_sa.json")
+  credentials = file("~/.config/gcloud/hometest-tf-admin.json")
   project     = var.google_project_id
   region      = var.region
   zone        = var.zone_sing_b
